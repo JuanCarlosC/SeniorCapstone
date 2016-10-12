7f0c1502311522f37b30b59e1bff753d8b2a4103
juancarlosc <jchave26@lion.lmu.edu>
2016-10-12 13:17:23 -0500

    added overview and a create manager option

--- wwwroot/manager/assets/js/controllers.js
+++ wwwroot/manager/assets/js/controllers.js
@@ -15,7 +15,105 @@ appManager.controller('appOverviewManger', ['$scope', '$routeParams', '$rootScop
 	function($scope, $rootScope, $routeParams) {
 		$scope.$root.tab = "overview";
 		$scope.title = "Overview";
-		$scope.featureView = 'data';
+		$scope.featureView = 'overview';
+		$scope.students = [{
+			'firstName': 'John',
+			'lastName': 'Gravokiean',
+			'gradeLevel': 'Senior',
+			'mentor': 'Jamie',
+			'profilePic': 'assets/imgs/profilepic.png'
+		}, {
+			'firstName': 'Luis',
+			'lastName': 'Chamaco',
+			'gradeLevel': 'Freshman',
+			'mentor': 'Louis',
+			'profilePic': 'assets/imgs/profilepic.png'
+		}, {
+			'firstName': 'Korina',
+			'lastName': 'Perez',
+			'gradeLevel': 'Sophmore',
+			'mentor': 'Josh',
+			'profilePic': 'assets/imgs/profilepic.png'
+		}, {
+			'firstName': 'Shanique',
+			'lastName': 'Doe',
+			'gradeLevel': 'Senior',
+			'mentor': 'Abbey',
+			'profilePic': 'assets/imgs/profilepic.png'
+		}, {
+			'firstName': 'John',
+			'lastName': 'Gravokiean',
+			'gradeLevel': 'Senior',
+			'mentor': 'Jamie',
+			'profilePic': 'assets/imgs/profilepic.png'
+		}, {
+			'firstName': 'Luis',
+			'lastName': 'Chamaco',
+			'gradeLevel': 'Freshman',
+			'mentor': 'Louis',
+			'profilePic': 'assets/imgs/profilepic.png'
+		}, {
+			'firstName': 'Korina',
+			'lastName': 'Perez',
+			'gradeLevel': 'Sophmore',
+			'mentor': 'Josh',
+			'profilePic': 'assets/imgs/profilepic.png'
+		}, {
+			'firstName': 'Shanique',
+			'lastName': 'Doe',
+			'gradeLevel': 'Senior',
+			'mentor': 'Abbey',
+			'profilePic': 'assets/imgs/profilepic.png'
+		},];
+		$scope.classes = [{
+			'name': 'Intro to Comp Sci',
+			'subject': 'information science',
+			'instructor': 'Dr. Reed',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'AP Geography',
+			'subject': 'geography',
+			'instructor': 'Mr. Mack',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'AP English',
+			'subject': 'english',
+			'instructor': 'Mr. Johnson',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'Algebra I',
+			'subject': 'math',
+			'instructor': 'Mr. Jagermenjenson',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		}, {
+			'name': 'Intro to Comp Sci',
+			'subject': 'information science',
+			'instructor': 'Dr. Reed',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'AP Geography',
+			'subject': 'geography',
+			'instructor': 'Mr. Mack',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'AP English',
+			'subject': 'english',
+			'instructor': 'Mr. Johnson',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'Algebra I',
+			'subject': 'math',
+			'instructor': 'Mr. Jagermenjenson',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		}];
 	}]);
 appManager.controller('appStudentsManager', ['$scope', '$routeParams', '$rootScope',
 	function($scope, $rootScope, $routeParams) {
@@ -79,6 +177,67 @@ appManager.controller('appClassesManager', ['$scope', '$routeParams', '$rootScop
 		$scope.$root.tab = "classes";
 		$scope.title = "Classes Module";
 		$scope.featureView = 'overview';
+
+		/* 
+		static hasMany = [students:Student]
+    String name
+    String subject
+    String instructor
+    Date startDate
+    Date endDate
+		*/
+
+		$scope.classes = [{
+			'name': 'Intro to Comp Sci',
+			'subject': 'information science',
+			'instructor': 'Dr. Reed',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'AP Geography',
+			'subject': 'geography',
+			'instructor': 'Mr. Mack',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'AP English',
+			'subject': 'english',
+			'instructor': 'Mr. Johnson',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'Algebra I',
+			'subject': 'math',
+			'instructor': 'Mr. Jagermenjenson',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		}, {
+			'name': 'Intro to Comp Sci',
+			'subject': 'information science',
+			'instructor': 'Dr. Reed',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'AP Geography',
+			'subject': 'geography',
+			'instructor': 'Mr. Mack',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'AP English',
+			'subject': 'english',
+			'instructor': 'Mr. Johnson',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		},{
+			'name': 'Algebra I',
+			'subject': 'math',
+			'instructor': 'Mr. Jagermenjenson',
+			'startDate': 'Wednesday, August 17, 2016',
+			'endDate': 'Friday, December 14, 2016'
+		}];
+
+
 	}]);
 appManager.controller('appClocktimeManager', ['$scope', '$routeParams', '$rootScope',
 	function($scope, $rootScope, $routeParams) {
